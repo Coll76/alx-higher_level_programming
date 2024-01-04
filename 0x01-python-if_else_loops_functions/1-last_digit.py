42131108234eb0c9ecf4e42a32cbb074c5611c9a
Coll76 <collins.kubu@gmail.com>
2024-01-04 10:26:53 -0800

    prints the ASCII alphabet, in lowercase, not followed by a new line.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,4 +12,3 @@ elif ((number % 10) == 0):
     print(f"Last digit of {number} is {number % 10} and is 0")
 elif (((number % 10) < 6) and ((number % 10) != 0)):
     print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
-
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+alphabet = ord('a')
+while(alphabet <= ord('z')):
+    print(chr(alphabet), end='')
+    alphabet += 1
