081d4998ce5c81714e9dd5cd1f43c364dd582f27
Coll76 <collins.kubu@gmail.com>
2024-01-04 12:19:24 -0800

    Write a program that prints numbers from 0 to 99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for numbers in range(0, 99 + 1):
+    if (numbers < 10):
+        print("0{}, ".format(numbers), end='')
+        pass
+    elif(numbers > 10):
+        print("{}".format(numbers), end=(', ' if numbers < 99 else "\n"))
