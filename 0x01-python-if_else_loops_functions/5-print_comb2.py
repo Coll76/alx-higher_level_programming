8d3deb73c8f6d2f95edca7c151feeec9a8fdb69e
Coll76 <collins.kubu@gmail.com>
2024-01-04 12:24:31 -0800

    prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,5 @@ for numbers in range(0, 99 + 1):
     if (numbers < 10):
         print("0{}, ".format(numbers), end='')
         pass
-    elif(numbers > 10):
+    elif (numbers >= 10):
         print("{}".format(numbers), end=(', ' if numbers < 99 else "\n"))
