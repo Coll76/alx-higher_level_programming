1c36cdba6d1af64eec0c18476c1a0c72ae4afbe3
Coll76 <collins.kubu@gmail.com>
2024-01-06 03:33:16 -0800

    prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 0:
-    print("0")
-else:
-    sums = 0
-    number = [int(arg) for arg in sys.argv[1:]]
-    sums = sum(number)
-    print(sums)
+if __name__ == "__main__":
+    if len(sys.argv) == 0:
+        print("0")
+    else:
+        sums = 0
+        number = [int(arg) for arg in sys.argv[1:]]
+        sums = sum(number)
+        print(sums)
