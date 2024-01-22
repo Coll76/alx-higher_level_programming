8262b18630c14af9fe7a23694a8083faad462401
Coll76 <collins.kubu@gmail.com>
2024-01-22 13:27:07 -0800

    function that raises a type exception

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,6 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     ne_list = []
     if len(my_list_1) < list_length or len(my_list_2) < list_length:
         print("out of range")
+    
     for i in range(len(my_list_1)):
         try:
             result = my_list_1[i] / my_list_2[i]
@@ -12,9 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
             ne_list.append(0)
         except TypeError:
             print("wrong type")
-            ne_list.append(0)
-        except IndexError:
-            print("out of range")
             ne_list.append(0)
         finally:
             pass
