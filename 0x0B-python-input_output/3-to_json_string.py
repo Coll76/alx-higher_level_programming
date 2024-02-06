6e47e049064f7035a48d3cf77f31e29173aff35a
Coll76 <collins.kubu@gmail.com>
2024-02-06 14:37:21 -0800

    function that returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ function that returns the JSON
+ representation of an object (string):
+ """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    returns the JSON
+    representation of an object (string)
+    known as serialization
+    """
+    obj = json.dumps(my_obj)
+    return obj
