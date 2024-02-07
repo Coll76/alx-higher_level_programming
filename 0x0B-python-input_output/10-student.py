5e7fb11eaf462f4e8b277753c4f9f66e6461f831
Coll76 <collins.kubu@gmail.com>
2024-02-06 18:40:19 -0800

    class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+class Student that defines a student by: (based on 9-student.py)
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        __init__ used for initialization
+        Args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        """
+        retrieves a dictionary representation of a Student
+        """
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student
+        """
+        if attrs is None:
+            return self.__dict__
+        result = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                result[attr] = getattr(self, attr)
+        return result
