59088d2bfb4f141182ef01c5a6bcdeacb7fb4ca5
Coll76 <collins.kubu@gmail.com>
2024-04-17 12:24:44 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,38 @@
 #!/usr/bin/python3
+
 import MySQLdb
 import sys
 
+"""
+Elimination of SQL  injection in such a way tht it does not occur
+"""
+
+
 def main():
+    """
+    Used to warp the data into a single unit
+    Accepts arguments from the console
+    Gets the password, username, database, state_name_searched
+    """
     if len(sys.argv) != 5:
         print("Invalid number of arguments")
         return
     username, password, database, state_name_searched = sys.argv[1:]
-    db = MySQLdb.connect(passwd = password, db = database, user = username, host = 'localhost', port = 3306)
+    db = MySQLdb.connect(
+            passwd=password,
+            db=database,
+            user=username,
+            host='localhost',
+            port=3306
+            )
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name = '+ state_name_searched)
+    cur.execute('SELECT * FROM states WHERE name = %s', (state_name_searched,))
     output = cur.fetchall()
     for ou in output:
         print(ou)
     cur.close()
     db.close()
+
+
 if __name__ == "__main__":
     main()
