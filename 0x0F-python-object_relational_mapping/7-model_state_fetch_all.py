85a0ddf18152b49cf72a0a7900de84b9b2631d8c
Coll76 <collins.kubu@gmail.com>
2024-04-18 16:48:05 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Modules imported to onnect to database server
+import to connect to server
 """
 
 import sys
@@ -13,18 +13,15 @@ from model_state import Base, State
 lists all State objects from the database hbtn_0e_6_usa
 take 3 arguments: mysql username, mysql password and database name
 import State and Base from model_state
-should connect to a MySQL server running on localhost at port 3306
-sorted in ascending order by states.id
+connect to a MySQL server running on localhost at port 3306
+ sorted in ascending order by states.id
 """
 
 
 def main():
     """
-    wraps up the code to a single unit
+    wraps code to single unit
     """
-    if len(sys.argv) != 4:
-        print('Invalid arguments')
-        return
     engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost/{}'.format(
                 sys.argv[1],
@@ -32,11 +29,14 @@ def main():
                 sys.argv[3]
                 ),
             pool_pre_ping=True)
-
-    Output = engine.execute('SELECT * FROM states ORDER BY states.id ASC')
-    for ro in Output:
-        print(ro)
-    Output.close()
+    output = engine.execute(
+            'SELECT * FROM {} ORDER BY id ASC'.format(
+                'states'
+                )
+            )
+    for ro in output:
+        print(f"{ro.id}: {ro.name}")
+    output.close()
 
 
 if __name__ == "__main__":
