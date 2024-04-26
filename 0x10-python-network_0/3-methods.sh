dce9e3d1cf6d7c784f90c320a6ad7de2ee201d31
Coll76 <collins.kubu@gmail.com>
2024-04-26 05:59:44 -0700

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -X OPTIONS -i -L --max-redirs 5 "$1"
+curl -X OPTIONS -i -L --max-redirs 5 "$1" | grep -i -o '^Allow: .*$'| awk '{print $2}'
