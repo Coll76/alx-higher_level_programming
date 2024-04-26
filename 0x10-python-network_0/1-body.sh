ef73f97ffc339cfaf19747d84241c0cbc501d420
Coll76 <collins.kubu@gmail.com>
2024-04-26 00:58:22 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-# Display only body of a 200 status code response
-# You have to use curl
-
-if [ $# != 1 ]; then
-	echo "Invalid number of arguments"
-	exit 1
-fi
-url=$1
-request=$(curl -s -w "%{http_code}" "$url")
-if [ "$request" == "200" ]; then
-	$(curl -s "$url")
-fi
+curl -s "$1"|grep -i 
