5276b1fb233a8568775b9c644943f675cb2b3ddb
Coll76 <collins.kubu@gmail.com>
2024-04-25 22:55:37 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-# The size must be displayed in bytes
-# You have to use curl
-# Please test your script in the sandbox provided, using the web server running on port 5000
-if [ $# != 1 ]; then
-	echo "Invalid arguments"
-	exit 98
-else
-	url=$1
-	size=$(curl -s -w "%{size_download}" "$url")
-	echo "$size"
-fi
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s -w "%{size_download}\n" $1
