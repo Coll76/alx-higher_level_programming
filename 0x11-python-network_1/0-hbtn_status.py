93673f08ede7ec1e1515d6aee1ffeaa343aa71fa
Coll76 <collins.kubu@gmail.com>
2024-04-26 23:12:16 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -16,6 +16,6 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
 
 print("Body response:")
-print("- type: ", type(html))
-print("- content: ", html)
-print("- utf8 content: ", html.decode('UTF-8'))
+print("    - type:", type(html))
+print("    - content:", html)
+print("    - utf8 content:", html.decode('UTF-8'))
