188e9b97685fc07e5c5384c20674880a2e1727b0
Coll76 <collins.kubu@gmail.com>
2024-04-26 23:01:19 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
+
+# You must use the package urllib
+# You are not allowed to import any packages other than urllib
+# The body of the response must be displayed like the following example (tabulation before -)
+# You must use a with statement
+
+import urllib.request
+
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    
+#parse_json_data = json.loads(html)
+
+print("Body response:")
+print("   - type: ", type(html))
+print("   - content: ", html)
+print("   - utf8 content: ", html.decode('UTF-8'))
