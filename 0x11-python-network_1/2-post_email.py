cb4317a85a91b43571afc80fb7e367df604db9d1
Coll76 <collins.kubu@gmail.com>
2024-04-29 06:58:30 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,41 +1,14 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL, sends a request to
-the URL and displays the body of the response (decoded in utf-8).
-
-You have to manage urllib.error.
-HTTPError exceptions and print: Error code: followed by the HTTP status code
-You must use the packages urllib and sys
-You are not allowed to import other packages than urllib and sys
-You don’t need to check arguments passed to the script (number or type)
-You must use the with statement
-"""
-
-import sys
-from urllib.request import HTTPRedirectHandler, build_opener, install_opener, urlopen
+import urllib.request
 import urllib.parse
+import sys
 
-"""
-takes in a URL, sends a request to
-the URL and displays the body of the response (decoded in utf-8).
-"""
+url = sys.argv[1]
+email = sys.argv[2]
+data = urllib.parse.urlencode({'Your email is': email})
+result = data.encode('utf-8')
+req = urllib.request.Request(url, data=result, method='POST')
 
-def main(HTTPRedirectHandler):
-    """
-    takes in a URL, sends a request to
-    the URL and displays the body of the response (decoded in utf-8)
-    """
-    if len(sys.argv) != 2:
-        print("Invalid arguments")
-        exit(98)
-
-    email = sys.argv[2]
-    data = urllib.parse.urlencode({'Your email is': email})
-    data = data.encode('UTF-8')
-    post = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(post) as request:
-        get = request.read()
-        print(get)
-
-    if __name__ == "__main__":
-        main()
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+    print(body)
