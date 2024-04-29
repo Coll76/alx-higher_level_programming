05c707fc43a5937900eceb16dbf2fefcaa4a0538
Coll76 <collins.kubu@gmail.com>
2024-04-29 05:37:12 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ You must use the with statement
 """
 
 import sys
-import urllib.request
+from urllib.request import HTTPRedirectHandler, build_opener, install_opener
 import urllib.parse
 
 """
@@ -20,14 +20,22 @@ takes in a URL, sends a request to
 the URL and displays the body of the response (decoded in utf-8).
 """
 
+def Redirecthandler(HTTPRedirectHandler):
+    """
+    takes in a URL, sends a request to
+    the URL and displays the body of the response (decoded in utf-8)
+    """
+    if len(sys.argv) != 2:
+        print("Invalid arguments")
+        exit(98)
 
-if len(sys.argv) != 2:
-    print("Invalid arguments")
-    exit 98
-email = sys.argv[2]
-data = urllib.parse.urlencode({'Your email is': email})
-data = data.encode('UTF-8')
-post = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(post) as request:
-    get = request.read()
-    print(get)
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'Your email is': email})
+    data = data.encode('UTF-8')
+    post = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(post) as request:
+        get = request.read()
+        print(get)
+
+    if __name__ == "__main__":
+        Redirecthandler()
