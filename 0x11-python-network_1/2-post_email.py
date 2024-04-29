193c56977e23fc499853b9beb369c6850852aa5c
Coll76 <collins.kubu@gmail.com>
2024-04-29 05:17:22 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
+Python script that takes in a URL, sends a request to
+the URL and displays the body of the response (decoded in utf-8).
 
-You have to manage urllib.error.HTTPError exceptions and print: Error code: followed by the HTTP status code
+You have to manage urllib.error.
+HTTPError exceptions and print: Error code: followed by the HTTP status code
 You must use the packages urllib and sys
 You are not allowed to import other packages than urllib and sys
 You don’t need to check arguments passed to the script (number or type)
