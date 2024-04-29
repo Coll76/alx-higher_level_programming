a00e63247de6a2090089dff9fd690e1628401c40
Coll76 <collins.kubu@gmail.com>
2024-04-29 00:50:28 -0700

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -L --max-redirs 5 -H "email: test@gmail.com" -H "subject: I will always be here for PLD" -X GET "$1"
+curl -s -L --max-redirs 5 -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
