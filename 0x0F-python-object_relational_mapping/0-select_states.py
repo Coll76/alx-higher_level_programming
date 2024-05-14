f22eb77140da5e003ed499adf29c091c14c88c8b
Coll76 <collins.kubu@gmail.com>
2024-05-14 00:50:31 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,15 @@ import sys
 if len(sys.argv) != 4:
     print("Invalid arguments")
 
-username, password, database =  sys.argv[1:]
+username, password, database = sys.argv[1:]
 
-db = MySQLdb.connect(host='localhost', user=username, db=database, port=3306, passwd=password)
+db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        db=database,
+        port=3306,
+        passwd=password
+        )
 cur = db.cursor()
 cur.execute('SELECT * FROM states ORDER BY states.id ASC')
 result = cur.fetchall()
