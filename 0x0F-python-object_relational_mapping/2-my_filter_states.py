0aa9ccff874e1e4d8c5f88a35559bdf4e83bb8dd
Coll76 <collins.kubu@gmail.com>
2024-05-14 01:49:15 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,42 +1,32 @@
 #!/usr/bin/python3
 """
- lists all states from the database hbtn_0e_0_usa
- take 3 arguments: mysql username, mysql password and database name
- must use the module
- should connect to a MySQL server running on localhost at port 3306
- Results must be sorted in ascending order by states.id
-lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
-takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
+use the module MySQLdb
 """
-
 import MySQLdb
 import sys
 
+if len(sys.argv) != 5:
+    print("Invalid arguments")
 
-def main():
-    """
-    connects to mysql server
-    """
-    if len(sys.argv) != 5:
-            return
-    username, password, database, nam = sys.argv[1:]
-    db = MySQLdb.connect(
-            host = 'localhost',
-            port = 3306,
-            user = username,
-            passwd = password,
-            db = database
-            )
-    cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name = %s ORDER BY id ASC', (nam,))
-    cur.execZZ
-    ro = cur.fetchall()
-    for r in ro:
-        print(r)
-    if cur:
-        cur.close()
-    if db:
-        db.close()
-if __name__ == '__main__':
-    main()
+username, password, database, state_name_searched = sys.argv[1:]
+
+db = MySQLdb.connect(
+        user=username,
+        host='localhost',
+        passwd=password,
+        port=3306,
+        db=database
+        )
+cur = db.cursor()
+cur.execute('''
+            SELECT *
+            FROM states
+            WHERE name="{}"
+            ORDER BY states.id ASC
+            '''.format(state_name_searched))
+
+result = cur.fetchall()
+for data in result:
+    print(data)
+cur.close()
+db.close()
