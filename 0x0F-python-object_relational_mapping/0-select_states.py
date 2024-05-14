a14f21da02dbc4189ca6219efa8ca19a63a721a1
Coll76 <collins.kubu@gmail.com>
2024-05-14 00:47:45 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
 """
- lists all states from the database hbtn_0e_0_usa
- take 3 arguments: mysql username, mysql password and database name
- must use the module
- should connect to a MySQL server running on localhost at port 3306
- Results must be sorted in ascending order by states.id
-
- """
-
+module MySQLdb for manipulation of database
+"""
 import MySQLdb
 import sys
 
-def main():
-    """
-    connects to mysql server
-    """
-    if len(sys.argv) != 4:
-            return
-    username, password, database = sys.argv[1:]
-    db = MySQLdb.connect(
-            host = 'localhost',
-            port = 3306,
-            user = username,
-            passwd = password,
-            db = database
-            )
-    cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY id ASC')
-    ro = cur.fetchall()
-    for r in ro:
-        print(r)
-    if 'cur' is not None:
-        cur.close()
-    db.close()
-if __name__ == '__main__':
-    main()
+if len(sys.argv) != 4:
+    print("Invalid arguments")
+
+username, password, database =  sys.argv[1:]
+
+db = MySQLdb.connect(host='localhost', user=username, db=database, port=3306, passwd=password)
+cur = db.cursor()
+cur.execute('SELECT * FROM states ORDER BY states.id ASC')
+result = cur.fetchall()
+for data in result:
+    print(data)
+cur.close()
+db.close()
